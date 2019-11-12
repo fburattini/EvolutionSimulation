0fb62bf8c1da2e45f76259d2fdf3005658b78f39
Filippo B <f.burattini97@gmail.com>
2019-11-12 17:49:56 +0100

    first commit

---
+++ EvolutionSimulationCore/EvolutionSimulationCore/Models/Gene.cs
@@ -0,0 +1,60 @@
+﻿using EvolutionSimulationCore.Random;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvolutionSimulationCore.Models
+{
+    public class Gene
+    {
+        public DNAString dna;
+
+        public Gene()
+        {
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            this.dna = new DNAString();
+        }
+    }
+
+    public class DNAString
+    {
+        public Nucleotide[] nucleotides;
+
+        public DNAString()
+        {
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            this.nucleotides = new Nucleotide[260869];
+            for (int i = 0; i < nucleotides.Length; i++)
+            {
+                nucleotides[i] = new Nucleotide();
+            }
+        }
+    }
+
+    public class Nucleotide
+    {
+        public int value;
+
+        public Nucleotide()
+        {
+            this.Initialize();
+        }
+
+        #region Private functions
+        private void Initialize()
+        {
+            this.value = RandomProvider.GetRandomNumber(0, 100);
+        }
+        #endregion
+    }
+}
---
+++ EvolutionSimulationCore/EvolutionSimulationCore/Random/RandomProvider.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvolutionSimulationCore.Random
+{
+    public static class RandomProvider
+    {
+        private static readonly string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        public static Int32 GetRandomNumber(Int32 minValue, Int32 maxValue)
+        {
+            if (minValue > maxValue)
+                throw new ArgumentOutOfRangeException("minValue");
+            if (minValue == maxValue) return minValue;
+            Int64 diff = maxValue - minValue;
+
+            RNGCryptoServiceProvider rngCsp = new RNGCryptoServiceProvider();
+            byte[] randomNumber = new byte[4];
+
+            while (true)
+            {
+                rngCsp.GetBytes(randomNumber);
+                UInt32 rand = BitConverter.ToUInt32(randomNumber, 0);
+
+                Int64 max = (1 + (Int64)UInt32.MaxValue);
+                Int64 remainder = max % diff;
+                if (rand < max - remainder)
+                {
+                    return (Int32)(minValue + (rand % diff));
+                }
+            }
+        }
+
+        public static char GetRandomCharacter()
+        {
+            int index = GetRandomNumber(0, chars.Length);
+            return chars[index];
+        }
+    }
+}
